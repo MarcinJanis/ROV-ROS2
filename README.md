268b6bc32a52005f7e2125b363d42558b068e4c5
Marcin Janis <janis.marcin02@gmail.com>
2025-12-22 19:38:16 +0100

    speckle nise simulation added

--- README.md
+++ README.md
@@ -1,27 +1,61 @@
-# OBJ models
-
-1. cool 3D object:
-   
-https://sketchfab.com/3d-models/antarctic-sponges-1e1a5f790d1b4a25bff6e711009c604a
-
-2. 
+# TODO:
 
 
-# TODO
+
+
 ___
+Mathematical/phisical descriptios
 
-# 1. Custom scenario
-Dir: `src/simulation/rov_stonefish/scenarios` file:  `rapture.scn` <br>
-**TODO:**
-- Add **seabed** - find model of textured seabed on net and import to scenario
-- Check if any other files need to be replaced, eg. in launch there is .yaml file
+1) Polar to cartesian transformation
+...
 
 
-# 2. Sonar configuration
-**TODO:**
-- check if all parameters are correspoing to Aracati dataset
-- check if textures are visible
+2) Speckle noise
+   
+2.1) Signal Representation (Coherent Component)
+
+Acoustic echoes in sonar are fundamentally waves. 
+
+While the input image $I_{in}(x,y)$ represents intensity (energy), the physical interference phenomena responsible for speckle occur in the amplitude domain (pressure).
+
+We model the input image as the coherent component (the determined structure of the seabed/objects):
+$$A_{coh}(x,y) = \sqrt{I_{in}(x,y)}$$
+
+This represents the magnitude of the coherent phasor, assuming a zero initial phase for simplicity.
+
+2.2)  Statistical Modeling of Scatterers (Incoherent Component)
+
+Speckle arises from the interference of sub-resolution scatterers (sand, roughness) within a resolution cell. 
+
+According to the Burckhardt model, this is a "random walk" in the complex plane.
+
+Instead of simulating $M$ individual scatterers iteratively, we apply the Central Limit Theorem. 
+
+The sum of $M$ random independent phasors converges to a complex Gaussian distribution.
+
+For a pixel with $M$ scatterers (where $M \sim U(M_{min}, M_{max})$), the noise components $u$ (real) and $v$ (imaginary) are generated as:$$u_{raw}, v_{raw} \sim \mathcal{N}(0, \sigma_{noise}^2 \cdot M)$$Here, $\sigma_{noise}^2 \cdot M$ represents the aggregate variance of the scatterers within that cell.
+
+2.3) Beam Geometry Simulation (Spatial Correlation) 
+
+Real sonar systems do not measure single points; they measure the convolution of the scene with the system's Point Spread Function (PSF).
+
+Range axis ($r$): The pulse is very short, resulting in high resolution (no blurring).
+
+Azimuth axis ($\theta$): The beam width is significant, causing returns to "smear" across the arc.
+
+We simulate this by convolving the raw white noise with an anisotropic Gaussian kernel (the approximated PSF):$$u_{corr} = u_{raw} * h(x,y), \quad v_{corr} = v_{raw} * h(x,y)$$Where the kernel $h(x,y)$ is defined by the beam width $\sigma_{\theta}$:$$h(x,y) = \delta(y) \cdot \frac{1}{\sqrt{2\pi}\sigma_{\theta}} e^{-\frac{x^2}{2\sigma_{\theta}^2}}$$Note: In the code, a normalization factor is applied after filtration to compensate for the peak amplitude loss inherent in discrete convolution
+
+2.4) Image Formation (Interference and Detection) 
+
+The total signal received by the transducer is the vector sum of the coherent structure (signal) and the spatially correlated incoherent scattering (noise):
+
+$$\mathbf{A}_{total} = (A_{coh} + u_{corr}) + i(v_{corr})$$
+
+Finally, the sonar system performs envelope detection to display the image. 
+Mathematically, this corresponds to calculating the squared modulus of the complex phasor:
+
+$$I_{out}(x,y) = |\mathbf{A}_{total}|^2 = (A_{coh} + u_{corr})^2 + v_{corr}^2$$
 
 
 
-
+3) Energy loss
---
+++ dataset_visualisation/dataset_utils.py
@@ -0,0 +1,122 @@
+import os
+import numpy as np
+import cv2
+from scipy.ndimage import gaussian_filter
+
+data_headers = {
+    'step_idx':0,
+    't':1,
+    'pos_x':2,
+    'pos_y':3,
+    'pos_z':4,
+    'orient_x':5,
+    'orient_y':6,
+    'orient_z':7,
+    'orient_w':8,
+    'dvl_vel_x':9,
+    'dvl_vel_y':10,
+    'dvl_vel_z':11,
+    'dvl_vel_x':12,
+    'alt':13,
+    'imu_orient_x':14,
+    'imu_orient_y':15,
+    'imu_orient_z':16,
+    'imu_gyro_x':17,
+    'imu_gyro_y':18,
+    'imu_gyro_z':19,
+    'imu_accel_x':20,
+    'imu_accel_y':21,
+    'imu_accel_z':22,
+}
+    
+
+
+
+def addSpeckleNoise(img, sigma = 0.5, m_min = 10, m_max = 200, beam_width = 5.0):
+    '''
+    Adds speckle noise to sonar image
+    Sources: 
+    https://www.researchgate.net/publication/4252645_Speckle_Simulation_Based_on_B-Mode_Echographic_Image_Acquisition_Model
+    '''
+
+    h, w = img.shape
+
+    # intensity to amplitude I ~ A^2
+    A = np.sqrt(img)
+
+    m = np.random.randint(m_min, m_max + 1, size=(h, w)) # number os scatters for each pixel
+
+    scale_map = sigma * np.sqrt(m)
+
+    u = np.random.normal(loc=0.0, scale=scale_map , size=(h, w)) # real component of noise - amplitude
+    v = np.random.normal(loc=0.0, scale=scale_map , size=(h, w)) # imaginary comonent of noise - phase
+
+    # Gaussian filter with wide kernel 
+    u_cor = gaussian_filter(u, sigma=(beam_width, 0))
+    v_cor = gaussian_filter(v, sigma=(beam_width, 0))
+
+    normalization_factor = np.sqrt(2 * np.pi * beam_width) # to keep energy for noise, despite gaussian blure
+
+    u = u_cor * normalization_factor
+    v = v_cor * normalization_factor
+
+    # Add amplitude values
+    A = A + u 
+
+    img_noised = A**2 + v**2 # sum and module of coherent component (real objects echo) and incoherent component (echo from scatters)
+
+    img_noised = img_noised.astype(np.uint8)
+
+    return img_noised
+
+def eneryLoss(img):
+    # gain pixels that are close to r_min, 
+    # weak pixels that are far
+    # it should be exponential 
+    # set gain_min = 0.3 -> worst case scenario - val = 0.3*val
+    pass
+
+def addBandReflects(img):
+    # add bands reflects that are visible in sonar images, like beam sof light with 
+    pass
+
+def Polar2Cartesian(img, r_min = 2.0, r_max = 30.0, theta_min = -65*np.pi/180, theta_max = 65*np.pi/180, out_shape = None):
+    # r - ranges
+    # t - theta - beam angle
+    nr, nt = img.shape 
+
+    if out_shape is None:
+        out_shape = (nr, 2*nr)
+
+    output = np.ones(out_shape, dtype=np.uint8)
+
+    # center of out img
+    cy = out_shape[0]
+    cx = out_shape[1]//2
+
+    # create mesh grid for output size
+    X, Y = np.meshgrid(np.arange(out_shape[1]), np.arange(out_shape[0]))
+
+    # shift coords to set (0, 0) in robot position 
+    X = (X - cx).astype(np.float32)
+    Y = (cy - Y).astype(np.float32)
+
+    # convert to real world vals
+    scale = r_max / cy
+    X = X * scale
+    Y = Y * scale
+
+    # create remapping -> r = sqrt(x^2 + y^2), theta = atan2(x, y)
+    R = np.sqrt(X**2 + Y**2)
+    T = np.arctan2(X, Y)
+
+    # convert to input image coords sys 
+    dR = (r_max - r_min) / nr 
+    dT = (theta_max - theta_min) / nt
+
+    R_map = (R - r_min)/dR
+    Theta_map = (T - theta_min)/dT
+
+    output = cv2.remap(img, R_map.astype(np.float32), Theta_map.astype(np.float32), interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=255)
+    
+    return output
